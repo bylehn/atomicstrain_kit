edf583fd6dfbfd598286803460a341ec1870839a
Fabian Byléhn <f.bylehn@gmail.com>
2024-07-03 17:07:37 -0500

    Refactor file paths and update imports

--- atomicstrain/data/files.py
+++ atomicstrain/data/files.py
@@ -10,6 +10,8 @@ Use as ::
 
 __all__ = [
     "MDANALYSIS_LOGO",  # example file of MDAnalysis logo
+    "REFERENCE_PDB",    # reference structure PDB file
+    "DEFORMED_PDB",     # deformed structure PDB file
 ]
 
 import importlib.resources
@@ -17,3 +19,5 @@ import importlib.resources
 data_directory = importlib.resources.files("atomicstrain") / "data"
 
 MDANALYSIS_LOGO = data_directory / "mda.txt"
+REFERENCE_PDB = data_directory / "cript_wt.pdb"
+DEFORMED_PDB = data_directory / "cript_g330t.pdb"
--- atomicstrain/main.py
+++ atomicstrain/main.py
@@ -1,15 +1,16 @@
 from atomicstrain import StrainAnalysis
 from atomicstrain.visualization import plot_shear_strains
+from atomicstrain.data.files import REFERENCE_PDB, DEFORMED_PDB
 import MDAnalysis as mda
 
 # Set up your analysis
-ref = mda.Universe("reference.pdb")
-defm = mda.Universe("deformed.pdb", "trajectory.xtc")
+ref = mda.Universe(REFERENCE_PDB)
+defm = mda.Universe(DEFORMED_PDB)  # Assuming you don't have a separate trajectory file
 residue_numbers = list(range(307, 398))
-R = 10
+min_neighbors = 3  # Using min_neighbors instead of R as per the updated StrainAnalysis
 
 # Run the analysis
-strain_analysis = StrainAnalysis(ref, defm, residue_numbers, R)
+strain_analysis = StrainAnalysis(ref, defm, residue_numbers, min_neighbors)
 strain_analysis.run()
 
 # Visualize results
